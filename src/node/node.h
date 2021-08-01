ae2d1b83c376a45efaa8176838077a8537823986
Ilya-Belyanov <belyanov.99@mail.ru>
2021-08-01 18:27:49 +0300

    Try link

--- src/node/node.h
+++ src/node/node.h
@@ -11,4 +11,17 @@ struct Node {
     const T& data() const;
 };
 
+template<typename T> Node<T>::Node(): _data(T()){}
+template<typename T> Node<T>::Node(T data): _data(new T(data)){}
+
+template<typename T> Node<T>::~Node()
+{
+    delete _data;
+    _data = nullptr;
+}
+
+template<typename T> const T& Node<T>::data() const
+{
+    return *_data;
+}
 #endif // NODE_H
